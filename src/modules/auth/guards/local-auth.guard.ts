385717b3302de5ffdab11610614de2dea7674c14
André Quintino <pandrequintino@gmail.com>
2022-07-03 15:18:32 +0100

    feat(auth): implement localauth guard

---
+++ src/modules/auth/guards/local-auth.guard.ts
@@ -0,0 +1,23 @@
+import {
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+class LocalAuthGuard extends AuthGuard('local') {
+	canActivate(context: ExecutionContext) {
+		return super.canActivate(context);
+	}
+
+	handleRequest(error, user) {
+		if (error || !user) {
+			throw new UnauthorizedException(error?.message);
+		}
+
+		return user;
+	}
+}
+
+export { LocalAuthGuard };
