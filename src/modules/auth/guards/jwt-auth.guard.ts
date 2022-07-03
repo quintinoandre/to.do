6dff199e4e5a374a76c58435d01a6cc67855bff7
André Quintino <pandrequintino@gmail.com>
2022-07-03 15:17:45 +0100

    feat(auth): implement jwtauth guard

---
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,46 @@
+import {
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+
+import { IS_PUBLIC_KEY } from '../decorators';
+import { UnauthorizedError } from '../errors';
+
+@Injectable()
+class JwtAuthGuard extends AuthGuard('jwt') {
+	constructor(private reflector: Reflector) {
+		super();
+	}
+
+	canActivate(context: ExecutionContext): Promise<boolean> | boolean {
+		const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+			context.getHandler(),
+			context.getClass(),
+		]);
+
+		if (isPublic) {
+			return true;
+		}
+
+		const canActivate = super.canActivate(context);
+
+		if (typeof canActivate === 'boolean') {
+			return canActivate;
+		}
+
+		const canActivatePromise = canActivate as Promise<boolean>;
+
+		return canActivatePromise.catch((error) => {
+			if (error instanceof UnauthorizedError) {
+				throw new UnauthorizedException(error?.message);
+			}
+
+			throw new UnauthorizedException();
+		});
+	}
+}
+
+export { JwtAuthGuard };
